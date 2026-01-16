d9ed1ba993e8315a9aba0131672286e6eb0b1849
OmarP26 <omar.perez@cetys.edu.mx>
2026-01-15 20:32:41 -0800

    Theme color configuration

--- app/components/nav-bar.vue
+++ app/components/nav-bar.vue
@@ -6,6 +6,7 @@
       </NuxtLink>
     </div>
     <div class="navbar-end">
+      <ThemeToggle />
       <a class="btn btn-accent">Sign In
         <Icon name="tabler:brand-github-copilot" size="24" />
       </a>
