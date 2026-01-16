657307c2f8b116ddb70c3057079aa8e3a20ba989
OmarP26 <omar.perez@cetys.edu.mx>
2026-01-15 19:14:52 -0800

    Configures tailwind, daisyUI and icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["./app/assets/css/main.css"],
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
   eslint: {
     config: {
       standalone: false,
