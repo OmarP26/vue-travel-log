2d6f76efee0eb5188d63256256eb15134cd5bfc2
OmarP26 <omar.perez@cetys.edu.mx>
2026-01-21 19:34:39 -0800

    Signout process

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col gap-4 justify-center items-center">
+    <span class="loading loading-ring loading-xl" />
+  </div>
+</template>
