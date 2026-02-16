3f535e0b1aca58479a9cd79eabdd24d9f196f537
OmarP26 <omar.perez@cetys.edu.mx>
2026-02-15 21:11:15 -0800

    Shows locations cards and add get api

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,15 +1,10 @@
 import type { DrizzleError } from "drizzle-orm";
 import slugify from "slug";
 import { InsertLocation } from "~~/lib/db/schema";
+import defineAuthenticatedEventHandler from "~/utils/define-authenticated-event-handler";
 import { findLocationByName, findUniqueSlug, insertLocation } from "../../lib/db/queries/location";
 
-export default defineEventHandler(async (event) => {
-  if (!event.context.user) {
-    return sendError(event, createError({
-      statusCode: 401,
-      statusMessage: "Unauthorized",
-    }));
-  }
+export default defineAuthenticatedEventHandler(async (event) => {
   const result = await readValidatedBody(event, InsertLocation.safeParse);
 
   if (!result.success) {
