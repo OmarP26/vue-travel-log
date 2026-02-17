98029410ec125ac4102cff06d1709ea891c00933
OmarP26 <omar.perez@cetys.edu.mx>
2026-02-16 21:48:45 -0800

    Adds map and light/dark configuration

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -72,8 +72,11 @@ function toggleSidebar() {
         />
       </div>
     </div>
-    <div class="flex-1">
+    <div class="flex flex-1 flex-col">
       <NuxtPage />
+      <Map
+        class="flex-1"
+      />
     </div>
   </div>
 </template>
